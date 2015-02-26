06e639b5a3ebc00909fb5c1edd401fb723d43d75
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2015-02-26 17:09:46 +0100

    Update testit.m
    Remove .nb ending to prevent code tagged as Mathematica code.

--- testit.m
+++ testit.m
@@ -1,10 +1,10 @@
 % test case: given expertise
 disp('// Test nb_expert function on functionality (any output is valid)')
 a = rand(1,21);
-filepath = 'example_expertise.nb';
+filepath = 'example_expertise';
 nb_output = nb_expert(a,filepath)
 
 % test case: error-handling (no expertise)
 disp('// Test error-handling for not existent expertise')
-filepath = 'notexistent_expertise.nb';
+filepath = 'notexistent_expertise';
 nb_output = nb_expert(a,filepath)
